df84018836184656de45151ca963dbe4aa630d0b
prakritidev <prakritidevverma@gmail.com>
2023-01-05 20:51:08 +0530

    - feat(init): nodejs and spring boot init

---
+++ javasever/src/test/java/com/prakritidev/verma/javasever/JavaseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakritidev.verma.javasever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaseverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
